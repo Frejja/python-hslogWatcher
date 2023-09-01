b3f7022f68358d5a303ae93d551c553d1570d3ee
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-01 20:14:58 +0200

    Fixed it checking in the wrong directory

--- utils/findLatestDirectory.py
+++ utils/findLatestDirectory.py
@@ -1,10 +1,11 @@
 # findLatestDirectory.py
-# Latest Creaeted Directory in logs for hearhtstone
+# Latest Created Directory in logs for hearthstone
 
 import os
 
 if __name__ == '__main__':
 	hsLogPath = 'K:\games\Hearthstone\Logs'
-	all_subdirs = [d for d in os.listdir(hsLogPath) if os.path.isdir(d)]
+-	all_subdirs = [d for d in os.listdir(hsLogPath) if os.path.isdir(d)]
++	all_subdirs = [os.path.join(hsLogPath, d) for d in os.listdir(hsLogPath) if os.path.isdir(os.path.join(hsLogPath, d))]
 	latest_subdir = max(all_subdirs, key=os.path.getmtime)
 	print(latest_subdir)
