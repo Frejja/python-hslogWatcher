1ee1a87109b64d2ebdcbeb1f3e13061c228b71a2
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-01 20:18:57 +0200

    Fixed copy paste error introduced in the code

--- utils/findLatestDirectory.py
+++ utils/findLatestDirectory.py
@@ -5,7 +5,6 @@ import os
 
 if __name__ == '__main__':
 	hsLogPath = 'K:\games\Hearthstone\Logs'
--	all_subdirs = [d for d in os.listdir(hsLogPath) if os.path.isdir(d)]
-+	all_subdirs = [os.path.join(hsLogPath, d) for d in os.listdir(hsLogPath) if os.path.isdir(os.path.join(hsLogPath, d))]
+	all_subdirs = [os.path.join(hsLogPath, d) for d in os.listdir(hsLogPath) if os.path.isdir(os.path.join(hsLogPath, d))]
 	latest_subdir = max(all_subdirs, key=os.path.getmtime)
 	print(latest_subdir)
