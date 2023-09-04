3993f83ddd485aa2b1635c25b762fc2a8f4d7ef8
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-04 18:31:49 +0200

    Refactored code to retrieve file names in the given directory and pass each file's full path as a string to the magic.from_file() function.

--- utils/getFiletype.py
+++ utils/getFiletype.py
@@ -1,3 +1,15 @@
 import magic
+import os
 
 directory = "C:\Users\Mikael\AppData\Local\Overwolf\BrowserCache\Cache\Cache_Data"
+
+# Get list of all files in the directory
+files = os.listdir(directory)
+
+# Iterate over the list of files
+for file in files:
+    # Construct full path
+    full_path = os.path.join(directory, file)
+    # Feed the full path to magic.from_file()
+    file_type = magic.from_file(full_path)
+    print(f"File: {file}, Type: {file_type}")
