f3ffc7a370319cd2890e13b21ac1fb1a4599bdf7
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-04 18:57:24 +0200

    Fixed a temp direcoty for data as it could not rea

--- utils/getFiletype.py
+++ utils/getFiletype.py
@@ -1,7 +1,7 @@
 import magic
 import os
 
-directory = "C:\Users\Mikael\AppData\Local\Overwolf\BrowserCache\Cache\Cache_Data"
+directory = r"C:\Users\Mikael\AppData\Local\Overwolf\BrowserCache\Cache\Cache_Data_temp"
 
 # Get list of all files in the directory
 files = os.listdir(directory)
@@ -12,4 +12,5 @@ for file in files:
     full_path = os.path.join(directory, file)
     # Feed the full path to magic.from_file()
     file_type = magic.from_file(full_path)
+    file_type.
     print(f"File: {file}, Type: {file_type}")
