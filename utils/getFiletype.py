a2fc06ae712efc6d7406842d8c135427cfa15e2c
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-04 19:06:27 +0200

    Rename the file to have the extension .zip and move it to the temp_directory if the file_type is "gzip compressed data".

--- utils/getFiletype.py
+++ utils/getFiletype.py
@@ -1,8 +1,9 @@
-
 import magic
 import os
+import shutil
 
 directory = "C:\Users\Mikael\AppData\Local\Overwolf\BrowserCache\Cache\Cache_Data_temp"
+temp_directory = r"h:\dev\temp"
 
 # Get list of all files in the directory
 files = os.listdir(directory)
@@ -16,3 +17,8 @@ for file in files:
     # Check if the file type is "gzip compressed data"
     if "gzip compressed data" in file_type:
         print(f"File: {file}, Type: {file_type}")
+        # Rename the file to have the extension .zip
+        new_file_name = f"{os.path.splitext(file)[0]}.zip"
+        os.rename(full_path, os.path.join(directory, new_file_name))
+        # Move the file to temp_directory
+        shutil.move(os.path.join(directory, new_file_name), os.path.join(temp_directory, new_file_name))
