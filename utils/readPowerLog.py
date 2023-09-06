f44010bbe592bd6870c4cf1d545ed9ae41771d68
Trasselsnodda <73627224+Frejja@users.noreply.github.com>
2023-09-06 14:30:53 +0200

    Refactored readPowerLog.py into a class with an asynchronous function that uses watchdog and logging to continuously monitor Power.log in the latest directory returned by findLatestDirectory, and added functionality to write changes to a log file with the current date and time as the filename. The main function is now asynchronous and no longer includes a try-except block for KeyboardInterrupt. Additionally, the monitor_power_log function now silently awaits changes in the log file path.

--- utils/readPowerLog.py
+++ utils/readPowerLog.py
@@ -1,18 +1,53 @@
 # readPowerLog.py
-# Read power.log from the latest directory
+# Watch and read power.log from the latest directory
 
 import os
+import sys
+import time
+import asyncio
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from datetime import datetime
 from findLatestDirectory import find_latest_directory
 
-def read_power_log():
-    latest_dir = find_latest_directory()
-    power_log_path = os.path.join(latest_dir, 'power.log')
+class PowerLogWatcher:
+    def __init__(self):
+        self.power_log = ""
+        self.latest_dir = find_latest_directory()
+        self.power_log_path = os.path.join(self.latest_dir, 'power.log')
+        self.log_file_path = os.path.join('h:\\dev\\logs', datetime.now().strftime('%Y-%m-%d_%H-%M-%S-Power.log'))
+        logging.basicConfig(filename=self.log_file_path, level=logging.INFO,
+                            format='%(asctime)s - %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
 
-    with open(power_log_path, 'r') as file:
-        power_log = file.read()
+    class PowerLogHandler(FileSystemEventHandler):
+        def __init__(self, watcher):
+            self.watcher = watcher
 
-    return power_log
+        def on_modified(self, event):
+            if event.src_path == self.watcher.power_log_path:
+                with open(self.watcher.power_log_path, 'r') as file:
+                    self.watcher.power_log = file.read()
+                logging.info(self.watcher.power_log)
+
+    async def monitor_power_log(self):
+        event_handler = self.PowerLogHandler(self)
+        observer = Observer()
+        observer.schedule(event_handler, self.latest_dir, recursive=True)
+        observer.start()
+        try:
+            while True:
+                await asyncio.sleep(1)
+        except Exception as e:
+            observer.stop()
+            logging.error("Error occurred: ", e)
+        observer.join()
+
+    def get_power_log(self):
+        return self.power_log
 
 if __name__ == '__main__':
-    power_log = read_power_log()
-    print(power_log)
+    power_log_watcher = PowerLogWatcher()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(power_log_watcher.monitor_power_log())
